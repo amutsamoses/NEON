4c1a0ba7a794e0038e96317208618cbaf2a22273
amutsamoses <amutsamoses@gmail.com>
2024-06-16 16:21:20 +0300

    token expiration date

--- src/index.ts
+++ src/index.ts
@@ -62,8 +62,6 @@ app.get("/ok", (c) => {
   return c.html(
     html`
       <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Kephar Moses' Restaurant</title>
         <style>
           body {
